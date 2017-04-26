3ea626d4d5e7547981397d0857eee914b4202319
tqian1 <tqian1@ualberta.ca>
2017-04-26 16:31:26 -0600

    changes to app.js and assets, bower install and npm install work with fixes

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("blurAdminFullstackApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,38 @@
 'use strict';
 
-angular.module('blurAdminFullstackApp', ['blurAdminFullstackApp.auth',
-    'blurAdminFullstackApp.admin', 'blurAdminFullstackApp.constants', 'ngCookies', 'ngResource',
-    'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap', 'validation.match'
+angular.module('blurAdminFullstackApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'ui.sortable',
+  'ngTouch',
+  'toastr',
+  'xeditable',
+  'ui.slimscroll',
+  'ngJsTree',
+  'angular-progress-button-styles',
+  'validation.match',
+  'smart-table',
+  'angular-loading-bar',
+  'angularSpinner',
+  'ui.toggle',
+
+  'blurAdminFullstackApp.auth',
+  'blurAdminFullstackApp.constants',
+  'blurAdminFullstackApp.resources',
+  'blurAdminFullstackApp.theme',
+  'blurAdminFullstackApp.pages'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
+    // $httpProvider.defaults.useXDomain = true;
+    // $httpProvider.defaults.withCredentials = true;
+    // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    // $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+    // $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
   });
